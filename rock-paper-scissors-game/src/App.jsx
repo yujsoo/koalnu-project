e3e5c1a2b07142020b5008d2e4823ee0d12116c3
유지수 <jsyu333@gmail.com>
2026-03-26 22:07:05 +0900

    feat: 가위, 바위, 보 클릭시 컴퓨터 이미지 랜덤하게 나오도록 추가

--- rock-paper-scissors-game/src/App.jsx
+++ rock-paper-scissors-game/src/App.jsx
@@ -25,17 +25,34 @@ import { useState } from 'react';
 function App() {
   // 가위 바위 보 초기 상태: 아무것도 내지 않은 상태 = null
   const [playerState, setPlayerState] = useState(null);
+  const [computerState, setComputerState] = useState(null)
 
+  // 사용자가 가위, 바위, 보 버튼 클릭했을때
   const handleClick = (el) => {
     setPlayerState(choice[el]);
+
+   let computerChoice = randomChoice();
+    setComputerState(computerChoice);
   }
+
+  const randomChoice = () => {
+    // object는 배열처럼 index 접근이 불가능
+    // key 배열로 변환이 필요
+    const keys = Object.keys(choice) // [scissors, rock, paper]
+
+    // 배열의 몇번째 선택을 위한 index값 구하기
+    let randomKey = keys[Math.floor(Math.random() * keys.length)]
+
+    return choice[randomKey]; //랜덤 값 최종 도출
+  }
+
 
   return (
     <main className={'container'}>
       <h1>가위바위보 게임</h1>
       <div className={'box-group'}>
         <Box title={'나'} item={playerState}/>
-        <Box title={'컴퓨터'}/>
+        <Box title={'컴퓨터'} item={computerState}/>
       </div>
       <div className={'button-group'}>
         <button onClick={() => handleClick('scissors')}>가위</button>
