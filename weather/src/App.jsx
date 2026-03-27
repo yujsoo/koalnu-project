3049c90531ba5a61363302c5a7e304388175a0fe
유지수 <jsyu333@gmail.com>
2026-03-28 03:58:15 +0900

    feat: 스타일 수정

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
 import Box from './components/Box'
-// 1.앱이 실행시 바로 현재 위치 기반의 날씨가 보인다.
 
 function App() {
   const [position, setPosition] = useState(null)
   const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const getLocation = () => {
     if (navigator.geolocation) {
@@ -18,28 +19,29 @@ function App() {
          setPosition({ lati, long });
          getWeatherByCurrentLocation(lati, long);
         });
-
-      //console.log(position)
     }
   }
 
 // 학교
   const getWeatherByCurrentLocation = async (lati, long) => {
+setLoading(true);
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=c3123e76f40546bce398999e12df2c88&units=metric`;
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
+    console.log(data)
   }
 
   useEffect (() => {
     getLocation()
-
   }, []);
 
   return (
     <>
-      <Box item={weather}/>
-      <button type="button" onClick={() => getLocation()} className="button">현재 위치</button>
+      <Box item={weather} loading={loading}/>
+      <div className={'button-group'}>
+        <button type="button" onClick={() => getLocation()} className="button">현재 위치</button>
+      </div>
     </>
   )
 }
--- weather/src/components/Box.jsx
+++ weather/src/components/Box.jsx
@@ -1,16 +1,17 @@
+import Loading from './Loading';
+
 const Box = ({ item }) => {
-  if (!item) return <div>로딩중...</div>;
 
   return (
-    <div className={`box`}>
-        <p>{item && item.name}</p>
-        <p>온도: {item.main.temp}°C</p>
-        <p>체감: {item.main.feels_like}°C</p>
-        <p>습도: {item.main.humidity}%</p>
-        <p>풍속: {item.wind.speed} m/s</p>
-     </div>
-  )
-}
+    <div className="box">
+      <p>{item && <img src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="weather icon"/>}</p>
+      <p>{item && item.name}</p>
+      <p>{item && item.weather[0].description}</p>
+      <p>최고: {item && item.main.temp_max}°C 최저: {item && item.main.temp_min}°C 체감: {item && item.main.feels_like}°C</p>
+      <p>습도: {item && item.main.humidity}% 풍속: {item && item.wind.speed} m/s</p>
+    </div>
+  );
+};
 
 export default Box;
 
