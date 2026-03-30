e042d7760557ff7a15425f20c382fefe845562a9
유지수 <jsyu333@gmail.com>
2026-03-30 20:43:02 +0900

    feat: [5. 라우터: 멀티 웹페이지를 만드는 법] 강의 관련 파일 추가

---
+++ router-example/src/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from './page/HomePage'
+import AboutPage from './page/AboutPage'
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+      <AboutPage/>
+    </div>
+  )
+}
+
+export default App
